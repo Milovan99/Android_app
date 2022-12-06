db231ff93a8abf5cf9085fbf0cf275fee5b2020d
Milovan99 <milovanjakovljevic78@gmail.com>
2022-12-06 19:54:14 +0100

    BMI

--- app/src/main/java/com/milovanjakovljevic/first/MainActivity.kt
+++ app/src/main/java/com/milovanjakovljevic/first/MainActivity.kt
@@ -1,14 +1,30 @@
 package com.milovanjakovljevic.first
 
-import android.graphics.Color
+
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.activity_main.view.*
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    rezultatText.visibility=View.INVISIBLE
 
-        window.decorView.setBackgroundColor(Color.GREEN)
+        izracunajButton.setOnClickListener{
+            var tezina = tezinaText.text.toString().toDouble()
+            var visina = visinaText.text.toString().toDouble()
+
+            var bmi = tezina / (visina * visina)
+
+            rezultatText.visibility=View.VISIBLE
+            rezultatText.text="BMI "+ String.format("%.2f",bmi)
+
+        }
+
     }
+
 }
